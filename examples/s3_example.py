20cfc3de056e44b0d5ed8113f63d596c6ea4b298
wondera <develop@wondera.ai>
2024-10-09 14:59:51 +0800

    add put_object

--- examples/s3_example.py
+++ examples/s3_example.py
@@ -6,8 +6,8 @@ s3 = S3Client()
 
 cdn_url = "https://d1n8x1oristvw.cloudfront.net"
 
-file_path = "/home/nemo/workspace/work/mtds/test.wav"
-s3_key = "streaming/lyrics_adjust/ea3c92f0e7464f2f9cbd297062c4ca4b/ea3c92f0e7464f2f9cbd297062c4ca4b.wav"
+# file_path = "/home/nemo/workspace/work/mtds/test.wav"
+# s3_key = "streaming/lyrics_adjust/ea3c92f0e7464f2f9cbd297062c4ca4b/ea3c92f0e7464f2f9cbd297062c4ca4b.wav"
 
 
 # 没有extra_args参数示例
@@ -15,13 +15,29 @@ s3_key = "streaming/lyrics_adjust/ea3c92f0e7464f2f9cbd297062c4ca4b/ea3c92f0e7464
 
 # 有extra_args参数示例
 # 文件通过浏览器下载时，文件名自动设置为“晴天_周杰伦_acc.wav”
-resp = s3.upload_to_s3(
-    file_path,
+# resp = s3.upload_to_s3(
+#     file_path,
+#     s3_key,
+#     extra_args={"ContentDisposition": 'attachment;filename="晴天_周杰伦_acc.wav"'},
+# )
+
+# print(f"url: {cdn_url}/{s3_key}")
+
+
+# s3_key = "streaming/lyrics_adjust/ea3c92f0e7464f2f9cbd297062c4ca4b/ea3c92f0e7464f2f9cbd297062c4c222.lyric"
+
+# resp = s3.put_object(
+#     "测试数据 test aaa".encode(),
+#     s3_key,
+# )
+# print(f"url: {cdn_url}/{s3_key}")
+
+
+s3_key = "streaming/lyrics_adjust/ea3c92f0e7464f2f9cbd297062c4ca4b/ea3c92f0e7464f2f9cbd297062c4c224.lyric"
+
+resp = s3.put_object(
+    "测试数据 test aaa22".encode(),
     s3_key,
-    extra_args={"ContentDisposition": 'attachment;filename="晴天_周杰伦_acc.wav"'},
+    ContentDisposition='attachment;filename="test.lyric"',
 )
-
-
-url = f"{cdn_url}/{s3_key}"
-
-print(url)
+print(f"url: {cdn_url}/{s3_key}")
